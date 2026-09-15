44531abfc6fba28c288b6cd153cca3789ff46245
Rishika-settyneni <rishikasettyneni@gmail.com>
2026-09-15 19:25:18 +0530

    Fix Vercel build: disable ESLint strict, handle fs module

---
+++ internflow-dashboard/craco.config.js
@@ -0,0 +1,15 @@
+const webpack = require('webpack');
+
+module.exports = {
+  webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.fallback = {
+        ...webpackConfig.resolve.fallback,
+        fs: false,
+        path: false,
+        crypto: false,
+      };
+      return webpackConfig;
+    },
+  },
+};
